b96eb457023f53d2a11338215f25c1cb57d00801
Eman <relelrify@gmail.com>
2024-11-29 13:49:18 +0200

    session 3

---
+++ session_3_Control_Flow/Untitled-1.py
@@ -0,0 +1,10 @@
+print(range(0,5,1))
+for i in range(1,6,2):
+    print(i)
+
+import turtle
+amy = turtle.Turtle()
+for side in range(4): #side = 1 side = 2 side = 3 side = 4
+    amy.forward(100)
+    amy.right(90)
+turtle.done()
